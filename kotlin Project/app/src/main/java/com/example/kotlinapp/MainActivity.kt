669649d94a596827ff8f2042fd64b65f55bbea4e
shiva <st172239@gmail.com>
2021-08-09 16:36:17 +0545

    bottom nav Added

--- kotlin Project/app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ kotlin Project/app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -9,12 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val actionBar = supportActionBar
+        actionBar!!.title = "First Activity"
         // get reference to button
         val btn = this.findViewById<Button>(R.id.button1)
         btn.setOnClickListener{
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
-
         }
 
     }
--- kotlin Project/app/src/main/java/com/example/kotlinapp/SecondActivity.kt
+++ kotlin Project/app/src/main/java/com/example/kotlinapp/SecondActivity.kt
@@ -2,10 +2,14 @@ package com.example.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.view.SupportActionModeWrapper
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val actionBar = supportActionBar
+        actionBar!!.title = "Second Activity"
+        actionBar.setDisplayHomeAsUpEnabled(true)
     }
 }
